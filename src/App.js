f278b557882a191bb8a7aedbf341eab2b7b370b2
Breno Machado <brenomachado0520@gmail.com>
2022-08-15 17:23:25 -0300

    Limpeza do create-app-react.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="corzinha">
-          Olá mundo.
-          <br/>
-          Utilizando React.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p> Hello, world <br/>
+    Programmed to work and not to feel <br/>
+    Not even sure that this is real</p>
   );
 }
 
