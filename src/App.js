882510534b3a525d127d22587c4daf0d45cac7ab
Breno Machado <brenomachado0520@gmail.com>
2022-08-29 17:51:35 -0300

    Adicionado pacote do react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
-    <>
-      <Home/>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
   );
 }
 
