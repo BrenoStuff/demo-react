7840ed63e59ae1aee887afeb39bf7e3ed6ecffe0
Breno Machado <brenomachado0520@gmail.com>
2022-09-05 17:52:08 -0300

    Adicionado menu com navegação e logotipo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
 import './Header.css'
+import { GiComputerFan } from 'react-icons/gi'
+import { Link, NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header id='header-nav'>
+    <header className='header-nav'>
         <div className='container'>
-          <div>Logo</div>
-          <div>Menu</div>
+          
+          <Link to='/' id='nav-logo'> 
+            <div className="logotype">
+                <GiComputerFan className="icon-logo"/>
+                <h1>Logo</h1>
+            </div>
+          </Link>
+
+          <nav>
+            <ul>
+              <li><NavLink to='/'>Home</NavLink></li>
+              <li><NavLink to='/contact'>Contact</NavLink></li>
+            </ul>
+          </nav>
+
         </div>
     </header>
   )
