a7f969a675dff8130e0c744f225d56c9b9ddf7d5
Breno Machado <brenomachado0520@gmail.com>
2022-09-12 17:49:28 -0300

    Aprendendo Hooks do react

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,29 @@
 import './Contact.css'
+import { useState, useEffect} from 'react' 
 import { Link } from 'react-router-dom'
 import MainContainer from '../components/MainContainer'
 import Header from "../components/Header"
 import Footer from '../components/Footer'
 
 const Contact = () => {
+
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('User')
+
+  useEffect(() => {
+    console.log(count)
+  }, [count])
+
   return (
     <>
     <Header/>
       <MainContainer>
-        <h1>Contact</h1>
+        <h1>Contador? {count}</h1>
+        <button onClick={() => setCount(count + 1)}>Adicionar</button><br/><br/>
         <p>Apenas um teste amigo.</p>
-
+        <h1>Olá {name}</h1>
+        <button onClick={() => setName('Breno')}>Breno</button><br/><br/>
+        <button onClick={() => setName('Allan')}>Allan</button><br/><br/><br/>
         <Link to='/'>Home</Link>
       </MainContainer>
     <Footer/>
