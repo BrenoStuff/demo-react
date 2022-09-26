f2d451dfb837fe11b5efa25f22dd4f76e08ad1a9
Breno Machado <brenomachado0520@gmail.com>
2022-09-26 15:38:19 -0300

    Adicionado função de deletar um usuario

--- src/components/CardComment.js
+++ src/components/CardComment.js
@@ -1,52 +1,75 @@
 import { Link } from 'react-router-dom'
+import {TbTrash as IconTrash} from 'react-icons/tb'
 
-const CardComment = ({avatarUrl, name, children, id}) => {
+const CardComment = ({avatarUrl, name, children, id, users, setUsers}) => {
 
-    const {card, avatar, box, userName, text} = style
+    const {card, avatar, box, userName, text, buttonLink} = style
+
+    const deleteUser = async (id) => {
+        const response = await fetch('http://localhost/PJ3/ifsp-api-php/user/delete',{
+            method: 'DELETE',
+            body: JSON.stringify({id: id})
+        })
+        const result = await response.json()
+        if(result?.success){
+            const usersFiltered = users.filter((user) => {return user.id !== id})
+            setUsers(usersFiltered)
+        } else {
+            console.error(result?.error)
+        }
+    }
   
     return (
-			<Link to={`/user/${id}`}>
+
 				<div style={card}>
           <img style={avatar} src={avatarUrl} alt="Foto do Fulano"/>
           <div style={box}>
-              <h1 style={userName}>{name}</h1>
-              <p style={text}>{children}</p>
+            <Link to={`/user/${id}`}>
+                <h1 style={userName}>{name}</h1>
+                <p style={text}>{children}</p>
+            </Link>
+            
+            <IconTrash style={buttonLink} onClick={() => deleteUser(id)} />
           </div>
       	</div>
-			</Link>
+
     )
   }
   
   const style = {
-      avatar: {
-          background: '#CCC',
-          borderRadius: '50%',
-          height: '50px',
-          width: '50px',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          margin: '0 10px 0 0'
-      },
-      card: {
-          width: '300px',
-          background: '#EEE',
-          display: 'flex',
-          padding: '10px',
-          borderRadius: '10px',
-          margin: '15px'
-      },
-      userName: {
-          margin: 0,
-          fontSize: '0.9rem'
-      },
-      text: {
-          margin: 0
-      },
-      box: {
-          width: '235px'
-      }
-  
+    avatar: {
+        background: '#CCC',
+        borderRadius: '50%',
+        height: '50px',
+        width: '50px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '0 10px 0 0'
+        },
+        card: {
+            width: '300px',
+            background: '#EEE',
+            display: 'flex',
+            padding: '10px',
+            borderRadius: '10px',
+            margin: '15px'
+        },
+        userName: {
+            margin: 0,
+            fontSize: '0.9rem'
+        },
+        text: {
+            margin: 0
+        },
+        box: {
+            width: '235px'
+        },
+        buttonLink: {
+        cursor: 'pointer',
+        padding: '5px',
+        borderRadius: '5px'
+    }
   }
   
   export default CardComment
